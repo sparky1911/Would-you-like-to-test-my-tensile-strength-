486ca34a171f1c59c6370c159e0dc3d0fe0225ec
shashank vikram <singhvikramshashank@gmail.com>
2022-02-24 02:22:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,63 +1,45 @@
-import streamlit as st
 import pandas as pd
-import shap
+import numpy as np
+import joblib
+import streamlit
 import pickle
 
+nnmodel = pickle.load(open('finalized_model.pkl', 'rb'))
 
-st.write("""
-# TENSILE STRENGTH PREDICTION FOR DIFFERENT STEEL ALLOYS
+def xgbr_prediction(CARBON,MANGANESE,SILICON,CHROMIUM,NICKEL,MOLYBDENUM,VANADIUM,NITROGEN,NIOBIUM,COBALT,TUNGSTEN,ALUMINIUM,TITANIUM):
+    pred_arr=np.array([CARBON,MANGANESE,SILICON,CHROMIUM,NICKEL,MOLYBDENUM,VANADIUM,NITROGEN,NIOBIUM,COBALT,TUNGSTEN,ALUMINIUM,TITANIUM])
+    preds=pred_arr.reshape(1,-1)
+    preds=preds.astype(int)
+    model_prediction=nnmodel.predict(preds)
+    return model_prediction
 
-""")
-st.write('---')
+def run():
+    streamlit.title("Tensile strength of steel")
+    html_temp="""
+    """
+    streamlit.markdown(html_temp)
 
-# Loads the Boston House Price Dataset
+    CARBON = streamlit.text_input('CARBON')
+    MANGANESE = streamlit.text_input('MANGANESE')
+    SILICON = streamlit.text_input('SILICON')
+    CHROMIUM = streamlit.text_input('CHROMIUM')
+
+    NICKEL = streamlit.text_input('NICKEL')
+    MOLYBDENUM = streamlit.text_input('MOLYBDENUM')
+    VANADIUM = streamlit.text_input('VANADIUM')
+    NITROGEN = streamlit.text_input('NITROGEN')
+    NIOBIUM = streamlit.text_input('NIOBIUM')
+    COBALT = streamlit.text_input('COBALT')
+    TUNGSTEN = streamlit.text_input('TUNGSTEN')
+    ALUMINIUM = streamlit.text_input('ALUMINIUM')
+    TITANIUM = streamlit.text_input('TITANIUM')
+
+    prediction=""
+    if streamlit.button("predict"):
+        prediction=xgbr_prediction(CARBON,MANGANESE,SILICON,CHROMIUM,NICKEL,MOLYBDENUM,VANADIUM,NITROGEN,NIOBIUM,COBALT,TUNGSTEN,ALUMINIUM,TITANIUM)
 
 
-# Sidebar
-# Header of Specify Input Parameters
-st.sidebar.header('Specify Input Parameters')
+        streamlit.success("Tensile strength is : {}".format(prediction))
 
-def user_input_features():
-    CARBON = st.number_input('CARBON',min_value=None, max_value=None)
-    MANGANESE = st.number_input('MANGANESE',min_value=None, max_value=None)
-    SILICON = st.number_input('SILICON',min_value=None, max_value=None)
-    CHROMIUM = st.number_input('CHROMIUM',min_value=None, max_value=None)
-
-    NICKEL = st.number_input('NICKEL',min_value=None, max_value=None)
-    MOLYBDENUM = st.number_input('MOLYBDENUM',min_value=None, max_value=None)
-    VANADIUM = st.number_input('VANADIUM',min_value=None, max_value=None)
-    NITROGEN = st.number_input('NITROGEN',min_value=None, max_value=None)
-    NIOBIUM = st.number_input('NIOBIUM',min_value=None, max_value=None)
-    COBALT = st.number_input('COBALT',min_value=None, max_value=None)
-    TUNGSTEN = st.number_input('TUNGSTEN',min_value=None, max_value=None)
-    ALUMINIUM = st.number_input('ALUMINIUM',min_value=None, max_value=None)
-    TITANIUM = st.number_input('TITANIUM',min_value=None, max_value=None)
-
-    data = {'CARBON': CARBON,
-            'MANGANESE': MANGANESE,
-            'SILICON': SILICON,
-            'CHROMIUM': CHROMIUM,
-
-            'NICKEL': NICKEL,
-            'MOLYBDENUM': MOLYBDENUM,
-            'VANADIUM': VANADIUM,
-            'NITROGEN': NITROGEN,
-            'NIOBIUM':NIOBIUM,
-            'COBALT': COBALT,
-            'TUNGSTEN': TUNGSTEN,
-            'ALUMINIUM': ALUMINIUM,
-            'TITANIUM': TITANIUM}
-    features = pd.DataFrame(data, index=[0])
-    return features
-
-df = user_input_features()
-model = pickle.load(open('finalized_model.pkl', 'rb'))
-pred=model.predict(df)
-
-# Print specified input parameters
-
-st.write(df)
-st.write('---')
-
-st.header('Predicted Tensile Strength')
-st.write(pred)
+if __name__=='__main__':
+    run()
